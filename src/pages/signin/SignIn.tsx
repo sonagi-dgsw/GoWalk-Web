9b1665d2d44d38a02a1a22ea5fe3a13cd2289cd1
daehyeong2 <baconbacon1231@gmail.com>
2025-09-30 17:20:49 +0900

    init: 프로젝트 생성

---
+++ src/pages/signin/SignIn.tsx
@@ -0,0 +1,13 @@
+import * as S from "./styles/styles.ts";
+import Button from "./components/Button.tsx";
+
+const SignIn = () => {
+    return (
+        <>
+            <Button />
+            <S.Button2>hello2</S.Button2>
+        </>
+    )
+}
+
+export default SignIn;
---
+++ src/pages/signin/components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <button>HELLO WORLD</button>
+}
+
+export default Button;
