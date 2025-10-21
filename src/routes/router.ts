b0bfd10d32f9a4851169f208c096b3b02def9c8b
parkhj2009 <eee0110010100@gmail.com>
2025-10-21 17:15:58 +0900

    feat: /walk 추가

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,16 @@
 import {createBrowserRouter} from "react-router";
 import SignIn from "../pages/signin/SignIn.tsx";
+import Walk from "../pages/walk/Walk.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/signin",
         Component: SignIn,
     },
+    {
+        path: "/walk",
+        Component: Walk,
+    }
 ])
 
 export default router;
