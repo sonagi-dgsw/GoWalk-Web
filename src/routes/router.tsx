47a5fadca5713e92cdb7ce0b24f409456c59d244
dlaguswls333 <sk01048745480@gmail.com>
2025-11-04 18:08:08 +0900

    홈 화면 기초 완성

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import Ranking from "../pages/ranking/Ranking";
+import Home from "../pages/home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Home />,
   },
   {
     path: "ranking",
