afa20b4453615e53332513f8b316c71a19713eb0
parkhj2009 <eee0110010100@gmail.com>
2025-12-12 19:00:30 +0900

    feat: 핫플레이스  추천

---
+++ src/pages/walk_start/components/HotPlace.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { 
+    CourseCard, 
+    LabelRow, 
+    LabelLeft, 
+    LabelRight, 
+    CategoryText, 
+    ImageThumb 
+} from "../styles/styles";
+
+interface HotPlaceProps {
+    imgUrl: string; 
+    name: string;
+    category: string;
+    distance: string;
+}
+
+
+const HotPlace = ({ imgUrl, name, category, distance }: HotPlaceProps) => {
+    return (
+        <CourseCard> 
+            {/* MapThumb 대신 ImageThumb 사용 */}
+            <ImageThumb imgUrl={imgUrl} /> 
+            
+            <LabelRow>
+                <LabelLeft>{name}</LabelLeft> 
+                <LabelRight>{distance}</LabelRight> 
+            </LabelRow>
+
+            <CategoryText>{category}</CategoryText>
+
+        </CourseCard>
+    );
+};
+
+export default HotPlace;
